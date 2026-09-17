587c6dd2997f12fb9d6b0bb401199689e7315821
Dana Gardy <danagardy755@gmail.com>
2026-09-17 16:00:11 +0300

    Fix blank startup and remove invalid local API default

--- android/app/src/main/java/com/kurdanime/app/MainActivity.java
+++ android/app/src/main/java/com/kurdanime/app/MainActivity.java
@@ -1,6 +1,36 @@
 package com.kurdanime.app;
-import android.app.*; import android.os.*; import android.webkit.*; import android.content.*; import android.net.Uri;
-public class MainActivity extends Activity{
- static final String API="http://10.0.2.2:8787/api";
- @Override public void onCreate(Bundle b){super.onCreate(b); WebView w=new WebView(this); w.getSettings().setJavaScriptEnabled(true);w.getSettings().setDomStorageEnabled(true);w.getSettings().setMediaPlaybackRequiresUserGesture(false);w.setWebViewClient(new WebViewClient());w.loadUrl("file:///android_asset/index.html?api="+Uri.encode(API));setContentView(w);}
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    private WebView web;
+
+    @Override public void onCreate(Bundle state) {
+        super.onCreate(state);
+        web = new WebView(this);
+        WebSettings s = web.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setDatabaseEnabled(true);
+        s.setMediaPlaybackRequiresUserGesture(false);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        web.setBackgroundColor(Color.rgb(8,18,29));
+        web.setWebChromeClient(new WebChromeClient());
+        web.setWebViewClient(new WebViewClient());
+        setContentView(web);
+        web.loadUrl("file:///android_asset/index.html");
+    }
+
+    @Override public void onBackPressed() {
+        if (web != null && web.canGoBack()) web.goBack();
+        else super.onBackPressed();
+    }
 }
