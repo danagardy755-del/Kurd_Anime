e4b6db33dd7b98155601de9f0a044cd6d8e41e8c
Dana Gardy <danagardy755@gmail.com>
2026-09-17 15:39:06 +0300

    Fix missing Uri import for Android build

--- android/app/src/main/java/com/kurdanime/app/MainActivity.java
+++ android/app/src/main/java/com/kurdanime/app/MainActivity.java
@@ -1,5 +1,5 @@
 package com.kurdanime.app;
-import android.app.*; import android.os.*; import android.webkit.*; import android.content.*;
+import android.app.*; import android.os.*; import android.webkit.*; import android.content.*; import android.net.Uri;
 public class MainActivity extends Activity{
  static final String API="http://10.0.2.2:8787/api";
  @Override public void onCreate(Bundle b){super.onCreate(b); WebView w=new WebView(this); w.getSettings().setJavaScriptEnabled(true);w.getSettings().setDomStorageEnabled(true);w.getSettings().setMediaPlaybackRequiresUserGesture(false);w.setWebViewClient(new WebViewClient());w.loadUrl("file:///android_asset/index.html?api="+Uri.encode(API));setContentView(w);}
